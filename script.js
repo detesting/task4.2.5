9c1fc25435261d02aed3d08ed8edd94d8d9b39eb
Anna Topilina <anna-topilina@mail.ru>
2022-01-16 12:45:55 +0300

    task

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-let input = document.querySelector('.input-project__input');
-let timer
+const debounce = (fn, debounceTime) => {
+    let timer;
+    return function (...arg){
+        clearTimeout(timer)
+        timer = setTimeout(() => fn.call(this, ...arg), debounceTime)
+    }
+};
 
+// Функция для debounce
+const searchFn = () => {
+    const searchValue = document.querySelector('.input-project__input').value.trim()
+    const divProject = document.querySelector('.input-project');
+    const dataRemove = document.querySelector('.input-datalist');
+
+    if (dataRemove){
+        divProject.removeChild(dataRemove)
+    }
+    if (searchValue.length > 0){
+        send(searchValue)
+    }
+}
+
+const inputSearch = document.querySelector('.input-project__input');
 // Листенер для инпута
-input.addEventListener('keyup', (e) => {
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-        let search = document.querySelector('.input-project__input').value
+inputSearch.addEventListener('input', debounce(searchFn, 200))
 
-        const div = document.querySelector('.input-project');
-
-        const dataRemove = document.querySelector('.input-datalist');
-        if (dataRemove){
-            div.removeChild(dataRemove)
-        }
-
-        if (search.length > 0){
-            send(search)
-        }
-    }, 200)
-})
-
-// Отправка запроса
-async function send(search) {
-    let response = await fetch(`https://api.github.com/search/repositories?q=${search}`)
-    await response.json().then((res) => list(res.items.filter((item, index) => index < 5)))
+const send = async (search) => {
+    let response = await fetch(`https://api.github.com/search/repositories?q=${search}&per_page=5`)
+    await response.json().then((res) => list(res.items))
 }
 
 // Функция для листенера пункта из списка
 // добавляет карточку репозитория
-function cardFn(item, e){
-    e.preventDefault();
+const cardFn = (item, event) => {
+    event.preventDefault();
 
     const cards = document.getElementById('cards');
     const card = document.createElement('div');
@@ -52,7 +55,7 @@ function cardFn(item, e){
 
     const close = document.createElement('div');
     close.classList.add('close');
-    close.addEventListener('click', (event) => deleteItem(event))
+    close.addEventListener('click', deleteItem)
 
     card.appendChild(cardText);
     card.appendChild(close);
@@ -63,33 +66,38 @@ function cardFn(item, e){
 }
 
 // Удаление карточки (функция для листенера у карточки)
-function deleteItem(e){
-    e.preventDefault();
+const deleteItem = (event) => {
+    event.preventDefault();
     let cards = document.querySelector('.cards');
-    cards.removeChild(e.target.parentNode)
+    cards.removeChild(event.target.parentNode)
+    event.target.removeEventListener('click', deleteItem)
 }
 
 // Создание выпадающего списка
-function list(items){
+const list = (items) => {
+    const divProject = document.querySelector('.input-project');
+    const dataRemove = document.querySelector('.input-datalist');
+    if (dataRemove){
+        divProject.removeChild(dataRemove)
+    }
+
+    const datalist = document.createElement('ul');
+    datalist.id = 'projects';
+    datalist.classList.add('input-datalist');
     if (items.length){
-        const div = document.querySelector('.input-project');
-        const dataRemove = document.querySelector('.input-datalist');
-        if (dataRemove){
-            div.removeChild(dataRemove)
-        }
-
-        const datalist = document.createElement('ul');
-        datalist.id = 'projects';
-        datalist.classList.add('input-datalist');
-
         items.forEach((item) => {
             let option = document.createElement('li');
             option.textContent = item.name;
             option.classList.add('input-option');
-            option.addEventListener('click', (e) => cardFn(item, e))
+            option.addEventListener('click', (event) => cardFn(item, event))
 
             datalist.appendChild(option);
         })
-        div.appendChild(datalist);
+    } else {
+        let option = document.createElement('li');
+        option.textContent = 'Ничего не найдено';
+        option.classList.add('input-option');
+        datalist.appendChild(option);
     }
+    divProject.appendChild(datalist);
 }
